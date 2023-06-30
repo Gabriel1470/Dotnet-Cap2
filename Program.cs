7bf5e77bd8a1f6292f06fbd932ddabfff839f2dd
Gabriel1470 <gabriel1470bg@gmail.com>
2023-06-30 15:13:16 +0200

    Making aa list

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("inserire 10 numeri:");
+int[] arryNumber = new int[10];
+int i = 0;
+
+while (i < arryNumber.Length)
+{
+    arryNumber[i] = int.Parse(Console.ReadLine());
+    i++;
+}
+
+Console.WriteLine("/------------------------------------------/");
+Console.WriteLine("Sto stampando l'elenco:");
+
+foreach (int number in arryNumber)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("/------------------------------------------/");
+Console.WriteLine("Sto stampando l'elenco al contrario:");
+
+Array.Reverse(arryNumber);
+foreach(int number in arryNumber)
+{
+    Console.WriteLine(number);
+}
+
